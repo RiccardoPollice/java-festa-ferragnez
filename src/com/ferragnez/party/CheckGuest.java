8693ccc40ffa2be99658c4810666b4b4366e4878
RiccardoPollice <riccardo.pollice17@gmail.com>
2023-12-05 15:32:02 +0100

    End work

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -1,2 +1,36 @@
-package com.ferragnez.party;public class CheckGuest {
+package com.ferragnez.party;
+import java.util.Scanner;
+
+public class CheckGuest {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Creo l'array di invitati
+        String[] invitati  = {"Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi", "Bebe Vio", "Luis", "Paradis Zarei", "Martina Maccherone", "Rachel Zeilic"};
+
+        // Chiedo all'utente come si chiama
+        System.out.println("Come ti chiami? ");
+        String userName = scanner.nextLine();
+
+        // Creo una flag che mi serve per vedere se l'utente è stato trovato
+        boolean found = false;
+
+        // Ciclo all'interno dell'array per controllare se il nome è presente
+        for (int i = 0; i < invitati.length; i++) {
+
+            // Confronto il nome da cercare con l'elemento corrente
+            if (userName.equalsIgnoreCase(invitati[i])) {
+                found = true;
+            }
+        }
+
+        // Alla fine del ciclo for guardo il valore della variabile found
+        if (!found) {
+            System.out.println(" Non puoi entrare");
+        } else {
+            System.out.println("Prego, puoi entrare");
+        }
+
+        scanner.close();
+    }
 }
